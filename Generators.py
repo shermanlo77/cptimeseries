481e865b78840a59bc33200edf89bfef1c26384b
king akanni <rilwan.ade1@gmail.com>
2019-11-08 08:23:13 +0000

    Hello World

---
+++ Generators.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+def Generator():
+
+    def __init__(self, generator_params):
+        self.generator = None
+    pass
+
+    def create_generator(self):
+
+        return self.generator
+
+    def __call__(self):
+        return self.create_generator()
