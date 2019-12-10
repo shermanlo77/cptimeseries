679f987fb109480ecd23993ab8ae0f3c7b96556c
shermanlo77 <shermanip18@gmail.com>
2019-12-10 10:38:36 +0000

    Further reparameterise

--- model/compound_poisson.py
+++ model/compound_poisson.py
@@ -6,7 +6,7 @@ import statsmodels.tsa.stattools as stats
 
 def simulate(poisson_rate, mean_gamma, dispersion, rng):
     z = rng.poisson(poisson_rate)
-    shape = z * dispersion
+    shape = z / dispersion
     if z > 0:
         scale = mean_gamma/shape
     else:
@@ -14,14 +14,14 @@ def simulate(poisson_rate, mean_gamma, dispersion, rng):
     y = rng.gamma(shape, scale=scale)
     return(y, z)
 
-rng = random.RandomState(np.uint32(372625180))
+rng = random.RandomState(np.uint32(372625178))
 n = 10000
 x = np.zeros(n)
 for i in range(n):
     x[i] = 0.8*math.sin(2*math.pi/365 * i) + rng.normal()
 
-parameter_0 = (0.098, 0.713, 0.01, 0.055) #poisson
-parameter_1 = (0.066, 0.46, 0.09, 0.8) #mean
+parameter_0 = (0.098, 0.713, 0.11, 0.355) #poisson
+parameter_1 = (0.066, 0.40, 0.24, 1.3) #mean
 parameter_2 = (0.07, 0.373) #dispersion
 
 y = np.zeros(n)
@@ -38,16 +38,19 @@ y[0], z[0] = simulate(poisson_rate_array[0], mean_gamma_array[0],
 for i in range(1,n):
     
     poisson_rate_array[i] = math.exp(parameter_0[0] * x[i]
-        + parameter_0[1] * math.log(poisson_rate_array[i-1])
+        + parameter_0[1] * (math.log(poisson_rate_array[i-1]) - parameter_0[3])
         + parameter_0[2] * (z[i-1] - poisson_rate_array[i-1])
+            / math.sqrt(poisson_rate_array[i-1])
         + parameter_0[3])
     
     exponent = parameter_1[0] * x[i] \
-        + parameter_1[1] *  math.log(mean_gamma_array[i-1]) \
+        + parameter_1[1] *  (math.log(mean_gamma_array[i-1]) - parameter_1[3]) \
         + parameter_1[3]
     if z[i-1] > 0:
         exponent +=\
-            parameter_1[2] * (y[i-1]/z[i-1] - mean_gamma_array[i-1])
+            parameter_1[2] * (y[i-1]/z[i-1] - mean_gamma_array[i-1]) \
+                / mean_gamma_array[i-1] \
+                / math.sqrt(dispersion_array[i-1]*z[i-1])
     mean_gamma_array[i] = math.exp(exponent)
     
     dispersion_array[i] = math.exp(parameter_2[0] * x[i]
