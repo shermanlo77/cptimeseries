a2ecbee2d4cb2347cec1a1644047005609e39c03
shermanlo77 <shermanip18@gmail.com>
2020-04-23 18:01:16 +0100

    Corrected name

--- script/wales_1/dual.py
+++ script/wales_1/dual.py
@@ -15,7 +15,7 @@ def main():
     n_sample = parser.parse_args().sample
 
     seed = random.SeedSequence(8823248378182807527309606642925586340)
-    name = "downscale"
+    name = "dual"
     path_here = pathlib.Path(__file__).parent.absolute()
     fitter = fit_downscale.FitterDownscaleDual(name, path_here, pool, seed)
     fitter.fit(dataset.Wales1Training(), n_sample)
