4574328417731ad39aa3157b82eefdde1310f315
shermanlo77 <shermanip18@gmail.com>
2020-06-09 15:30:32 +0100

    Comment print.py

--- compound_poisson/print.py
+++ compound_poisson/print.py
@@ -1,3 +1,8 @@
+"""Functions for printing TimeSeries, Downscale, Forecast objects.
+
+These functions could be made to methods for the corresponding classes...
+"""
+
 import math
 import os
 from os import path
@@ -17,10 +22,25 @@ from compound_poisson import roc
 import dataset
 
 RAIN_THRESHOLD_ARRAY = [0, 5, 10, 15]
-RAIN_THRESHOLD_EXTREME_ARRAY = [0, 10, 20, 30]
+RAIN_THRESHOLD_EXTREME_ARRAY = [0, 5, 10, 15]
 
 def time_series(time_series, directory, prefix=""):
+    """Print figures for a given TimeSeries object
 
+    Save figure for precipitation time series
+    Save figure for cumulative frequency of precipitation in the time series
+    Save figures for sample autocorrelation and partial autocorrelation
+    Save figures for the poisson rate, gamma mean, gamma dispersion, latent
+        variable Z over time
+    Save the TimeSeries in text (converted to string which shows parameters)
+
+    Args:
+        time_series: compound_poisson.TimeSeries object
+        directory: where to save the figures
+        prefix: what to name the figures
+    """
+
+    #required when plotting times on an axis
     pandas.plotting.register_matplotlib_converters()
 
     colours = matplotlib.rcParams['axes.prop_cycle'].by_key()['color']
@@ -36,12 +56,14 @@ def time_series(time_series, directory, prefix=""):
     gamma_mean_array = time_series.gamma_mean.value_array
     gamma_dispersion_array = time_series.gamma_dispersion.value_array
 
+    #get autocorrelations
     acf = stattools.acf(y, nlags=20, fft=True)
     try:
         pacf = stattools.pacf(y, nlags=20)
     except(stattools.LinAlgError):
         pacf = np.full(21, np.nan)
 
+    #print precipitation time series
     plt.figure()
     ax = plt.gca()
     ax.set_prop_cycle(cycle)
@@ -51,6 +73,8 @@ def time_series(time_series, directory, prefix=""):
     plt.savefig(path.join(directory, prefix + "rainfall.pdf"))
     plt.close()
 
+    #print precipitation cumulative frequency
+    #draw dot for mass at 0 mm
     plt.figure()
     ax = plt.gca()
     ax.set_prop_cycle(cycle)
@@ -69,6 +93,7 @@ def time_series(time_series, directory, prefix=""):
     plt.savefig(path.join(directory, prefix + "cdf.pdf"))
     plt.close()
 
+    #plot sample autocorrelation
     plt.figure()
     ax = plt.gca()
     ax.set_prop_cycle(cycle)
@@ -80,6 +105,7 @@ def time_series(time_series, directory, prefix=""):
     plt.savefig(path.join(directory, prefix + "acf.pdf"))
     plt.close()
 
+    #plot sample partial autocorrelation
     plt.figure()
     ax = plt.gca()
     ax.set_prop_cycle(cycle)
@@ -91,6 +117,7 @@ def time_series(time_series, directory, prefix=""):
     plt.savefig(path.join(directory, prefix + "pacf.pdf"))
     plt.close()
 
+    #plot the poisson rate over time
     plt.figure()
     ax = plt.gca()
     ax.set_prop_cycle(cycle)
@@ -100,6 +127,7 @@ def time_series(time_series, directory, prefix=""):
     plt.savefig(path.join(directory, prefix + "poisson_rate.pdf"))
     plt.close()
 
+    #plot the gamma mean over time
     plt.figure()
     ax = plt.gca()
     ax.set_prop_cycle(cycle)
@@ -109,6 +137,7 @@ def time_series(time_series, directory, prefix=""):
     plt.savefig(path.join(directory, prefix + "gamma_mean.pdf"))
     plt.close()
 
+    #plot the gamme dispersion over time
     plt.figure()
     ax = plt.gca()
     ax.set_prop_cycle(cycle)
@@ -118,6 +147,7 @@ def time_series(time_series, directory, prefix=""):
     plt.savefig(path.join(directory, prefix + "gamma_dispersion.pdf"))
     plt.close()
 
+    #plot the latent variable z over time
     plt.figure()
     ax = plt.gca()
     ax.set_prop_cycle(cycle)
@@ -127,12 +157,24 @@ def time_series(time_series, directory, prefix=""):
     plt.savefig(path.join(directory, prefix + "z.pdf"))
     plt.close()
 
+    #print the parameters in text
     file = open(path.join(directory, prefix + "parameter.txt"), "w")
     file.write(str(time_series))
     file.close()
 
 def get_year_index_dir(time_array):
-    year_index_dir = {} #key: years #value: index of times with that year
+    """For splitting the time_array into different years, used for plotting
+        figures for different years
+
+    Args:
+        time_array: array of dates
+
+    Return:
+        dictionary, keys are years, values are slice objects pointing to that
+            year
+    """
+    #key: years #value: index of times with that year
+    year_index_dir = {}
     for i, time in enumerate(time_array):
         year = time.year
         if not year in year_index_dir:
@@ -144,27 +186,54 @@ def get_year_index_dir(time_array):
     return year_index_dir
 
 def forecast(forecast, observed_rain, directory, prefix=""):
+    """For plotting compound_poisson.forecast.time_series.Forecaster objects
 
+    Plots the forecasts for each year:
+        -forecast (mean) with 68% credible interval vs observe
+        -forecast (median) with 68% credible interval vs observe
+        -residuals
+        -ROC curve
+        -forecast probability of more than some amount (see
+            RAIN_THRESHOLD_ARRAY) of precipitation
+    Plots:
+        -area under ROC curve for each year
+        -root mean square error for each year
+
+    Args:
+        forecast: Forecaster object
+        observed_rain: numpy array of observed precipitation
+        directory: where to save the figure
+        prefix: what to name the figure
+    """
+
+    #required to load forecast samples
     forecast.load_memmap("r")
 
+    #required for plotting dates
     pandas.plotting.register_matplotlib_converters()
 
+    #setting plotting properties (for both observed and prediction)
     colours = matplotlib.rcParams['axes.prop_cycle'].by_key()['color']
     cycle_forecast = cycler.cycler(color=[colours[1], colours[0]],
                                    linewidth=[1, 1],
                                    alpha=[1, 0.5])
 
+    #split time_array into different years
     time_array = forecast.time_array
     year_index_dir = get_year_index_dir(time_array)
     year_array = np.asarray(list(year_index_dir))
 
-    rmse_array = []
+    rmse_array = [] #array for storing rmse for each year
+    #dictionary of AUC for different thresholds, key is amount of rain, value is
+        #array containing AUC for each year
     auc_array = {}
     for rain in RAIN_THRESHOLD_ARRAY:
         auc_array[rain] = []
 
+    #for each year
     for year, index in year_index_dir.items():
 
+        #slice the current forecast and observation and plot
         forecast_sliced = forecast[index]
         observed_rain_i = observed_rain[index]
         rmse_array.append(forecast_sliced.get_error_rmse(observed_rain_i))
@@ -175,6 +244,7 @@ def forecast(forecast, observed_rain, directory, prefix=""):
         forecast_upper_error = forecast_sliced.forecast_sigma[1]
         time_array_i = forecast_sliced.time_array
 
+        #plot forecast and observation time series
         plt.figure()
         ax = plt.gca()
         ax.set_prop_cycle(cycle_forecast)
@@ -190,6 +260,7 @@ def forecast(forecast, observed_rain, directory, prefix=""):
             path.join(directory, prefix + "_forecast_" + str(year) + ".pdf"))
         plt.close()
 
+        #plot forecast using median instead
         plt.figure()
         ax = plt.gca()
         ax.set_prop_cycle(cycle_forecast)
@@ -206,6 +277,7 @@ def forecast(forecast, observed_rain, directory, prefix=""):
                       prefix+"_forecast_median_"+str(year)+".pdf"))
         plt.close()
 
+        #plot residual
         plt.figure()
         plt.plot(time_array_i, forecast_i - observed_rain_i)
         plt.xlabel("time")
@@ -214,6 +286,7 @@ def forecast(forecast, observed_rain, directory, prefix=""):
             path.join(directory, prefix + "_residual_" + str(year) + ".pdf"))
         plt.close()
 
+        #plot ROC curve, save AUC as well
         plt.figure()
         for rain in RAIN_THRESHOLD_ARRAY:
             roc_curve = forecast_sliced.get_roc_curve(rain, observed_rain_i)
@@ -227,6 +300,8 @@ def forecast(forecast, observed_rain, directory, prefix=""):
         plt.savefig(path.join(directory, prefix + "_roc_" + str(year) + ".pdf"))
         plt.close()
 
+        #plot probability of more than rain precipitation
+        #plot vertical red line to show when that even actually happened
         for rain in RAIN_THRESHOLD_ARRAY:
             plt.figure()
             plt.plot(time_array_i, forecast_sliced.get_prob_rain(rain))
@@ -240,6 +315,7 @@ def forecast(forecast, observed_rain, directory, prefix=""):
                           prefix+"_prob_"+str(rain)+"_"+str(year)+".pdf"))
             plt.close()
 
+    #plot auc for each year
     plt.figure()
     for rain in RAIN_THRESHOLD_ARRAY:
         label = str(rain) + " mm"
@@ -252,6 +328,7 @@ def forecast(forecast, observed_rain, directory, prefix=""):
     plt.savefig(path.join(directory, prefix + "_auc.pdf"))
     plt.close()
 
+    #plot rmse for each year
     plt.figure()
     plt.plot(year_array, rmse_array, '-o')
     plt.xlabel("year")
@@ -259,6 +336,7 @@ def forecast(forecast, observed_rain, directory, prefix=""):
     plt.savefig(path.join(directory, prefix + "_rmse.pdf"))
     plt.close()
 
+    #plot roc curve for the entire test set
     plt.figure()
     for rain in RAIN_THRESHOLD_EXTREME_ARRAY:
         roc_curve = forecast.get_roc_curve(rain, observed_rain)
@@ -268,19 +346,55 @@ def forecast(forecast, observed_rain, directory, prefix=""):
     plt.savefig(path.join(directory, prefix + "_roc_all.pdf"))
     plt.close()
 
+    #memmap of forecasts no longer needed
     forecast.del_memmap()
 
 def downscale_forecast(forecast_array, test_set, directory, pool):
+    """For plotting compound_poisson.forecast.downscale.Forecaster objects
+
+    Plots:
+        -plots the spatial forecast map for each time point (in parallel)
+        -plots rmse spatial map
+        -plots forecast time series for each spatial point (in parallel)
+        -for each year:
+            -plot roc curve (use entire area)
+        -plots auc for each year
+        -plots rmse for each year
+        -plots roc curve using entire test set
+
+    Args:
+        forecast_array: compound_poisson.forecast.downscale.Forecaster object
+        test_set: Data object
+        directory: where to save the figures
+        pool: multiprocess object to do tasks in parallel
+    """
+
+    #required to load memmap containing forecasts
     forecast_array.load_memmap("r")
-    forecast_map = ma.empty_like(test_set.rain)
+
     downscale = forecast_array.downscale
+
+    angle_resolution = dataset.ANGLE_RESOLUTION
+    longitude_grid = test_set.topography["longitude"] - angle_resolution / 2
+    latitude_grid = test_set.topography["latitude"] + angle_resolution / 2
+    rain_units = test_set.rain_units
+
+    #forecast map, 3 dimensions, same as test set rain, prediction of
+        #precipitation for each point in space and time, 0th dimension is time,
+        #remaining is space
+    forecast_map = ma.empty_like(test_set.rain)
+    #root mean square error, 2 dimension, for each point in space
     rmse_map = ma.empty_like(test_set.rain[0])
-    year_index_dir = get_year_index_dir(forecast_array.time_array)
-    year_array = np.asarray(list(year_index_dir))
+    #area under curve, dictionary, keys: for each precipitation in
+        #RAIN_THRESHOLD_ARRAY, value: array, auc for each year
     auc_array = {}
+    #root mean square error for each year
     rmse_array = []
     for rain in RAIN_THRESHOLD_ARRAY:
         auc_array[rain] = []
+
+    year_index_dir = get_year_index_dir(forecast_array.time_array)
+    year_array = np.asarray(list(year_index_dir))
 
     series_dir = path.join(directory, "series_forecast")
     if not path.isdir(series_dir):
@@ -289,6 +403,7 @@ def downscale_forecast(forecast_array, test_set, directory, pool):
     if not path.isdir(map_dir):
         os.mkdir(map_dir)
 
+    #get forecast (median) and rmse for the maps
     for time_series_i, observed_rain_i in (
         zip(downscale.generate_unmask_time_series(),
             test_set.generate_unmask_rain())):
@@ -298,12 +413,7 @@ def downscale_forecast(forecast_array, test_set, directory, pool):
         forecast_map[:, lat_i, long_i] = forecaster.forecast_median
         rmse_map[lat_i, long_i] = forecaster.get_error_rmse(observed_rain_i)
 
-    #plot the rain
-    angle_resolution = dataset.ANGLE_RESOLUTION
-    longitude_grid = test_set.topography["longitude"] - angle_resolution / 2
-    latitude_grid = test_set.topography["latitude"] + angle_resolution / 2
-
-    rain_units = test_set.rain_units
+    #plot the spatial forecast for each time (in parallel)
     message_array = []
     for i, time in enumerate(test_set.time_array):
         title = "precipitation (" + rain_units + ") : " + str(time)
@@ -328,7 +438,7 @@ def downscale_forecast(forecast_array, test_set, directory, pool):
     plt.savefig(path.join(directory, "rmse_map.pdf"))
     plt.close()
 
-    #plot the forecast for each location
+    #plot the forecast (time series) for each location (in parallel)
     message_array = []
     for time_series_i, observed_rain_i in (
         zip(downscale.generate_unmask_time_series(),
@@ -338,6 +448,7 @@ def downscale_forecast(forecast_array, test_set, directory, pool):
         message_array.append(message)
     pool.map(PrintForecastSeriesMessage.print, message_array)
 
+    #for each year, plot roc curve, get rmse
     for (year, index) in year_index_dir.items():
         plt.figure()
         roc_curve_array = forecast_array.get_roc_curve_array(
@@ -353,6 +464,7 @@ def downscale_forecast(forecast_array, test_set, directory, pool):
         plt.savefig(path.join(directory, "test_roc_"+str(year)+".pdf"))
         plt.close()
 
+        #work out root mean square over space
         rmse_array_i = []
         for time_series_i, observed_rain_i in (
             zip(downscale.generate_unmask_time_series(),
@@ -364,11 +476,13 @@ def downscale_forecast(forecast_array, test_set, directory, pool):
             rmse_array_i.append(forecast_sliced_i.get_error_rmse(
                 observed_rain_sliced_i))
             forecaster_i.del_memmap()
+        #root mean square, so need to sum the squares
         rmse_array.append(math.sqrt(np.mean(np.square(rmse_array_i))))
 
+    #plot auc for each year
     plt.figure()
     for rain in RAIN_THRESHOLD_ARRAY:
-        label = str(rain) + " mm"
+        label = str(rain) + " " + rain_units
         auc = np.asarray(auc_array[rain])
         is_number = np.logical_not(np.isnan(auc))
         plt.plot(year_array[is_number], auc[is_number], '-o', label=label)
@@ -378,13 +492,15 @@ def downscale_forecast(forecast_array, test_set, directory, pool):
     plt.savefig(path.join(directory, "auc.pdf"))
     plt.close()
 
+    #plot rmse for each year
     plt.figure()
     plt.plot(year_array, rmse_array, '-o')
     plt.xlabel("year")
-    plt.ylabel("root mean square error (mm)")
+    plt.ylabel("root mean square error (" + rain_units + ")")
     plt.savefig(path.join(directory, "rmse.pdf"))
     plt.close()
 
+    #plot roc curve over the entire test set
     plt.figure()
     roc_curve_array = forecast_array.get_roc_curve_array(
         RAIN_THRESHOLD_EXTREME_ARRAY, test_set)
@@ -395,6 +511,8 @@ def downscale_forecast(forecast_array, test_set, directory, pool):
     plt.close()
 
 class PrintForecastMapMessage(object):
+    """For printing forecast over space for a given time point (in parallel)
+    """
 
     def __init__(self,
                  forecast_i,
@@ -426,6 +544,8 @@ class PrintForecastMapMessage(object):
         plt.close()
 
 class PrintForecastSeriesMessage(object):
+    """For printing forecast time series for a given spatial point (in parallel)
+    """
 
     def __init__(self, series_dir, time_series_i, observed_rain_i):
         self.series_sub_dir =  path.join(series_dir, str(time_series_i.id))
