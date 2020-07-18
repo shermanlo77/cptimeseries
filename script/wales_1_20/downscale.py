e0c90dea6f6a36af2980c976feffa154183fc3be
statrita2004 <statrita2004@gmail.com>
2020-07-18 01:59:49 +0100

    Adding downscaling codes for wales 1 and 5

---
+++ script/wales_1_20/downscale.py
@@ -0,0 +1,23 @@
+import argparse
+
+from numpy import random
+
+from compound_poisson import fit
+from compound_poisson import multiprocess
+import dataset
+
+def main():
+    pool = multiprocess.MPIPoolExecutor()
+
+    parser = argparse.ArgumentParser(description="Sample size")
+    parser.add_argument("--sample", help="number of mcmc samples", type=int)
+    n_sample = parser.parse_args().sample
+
+    seed = random.SeedSequence(96402363659177019578667127138226980756)
+    fitter = fit.downscale.FitterDownscale()
+    fitter.fit(dataset.Wales1Test(), seed, n_sample, pool)
+
+    pool.join()
+
+if __name__ == "__main__":
+    main()
