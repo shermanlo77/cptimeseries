shermanlo77/cptimeseries

commit a0da80d845d9af1bdf9a35a7606839f111daa818

memmap reading is done in TimeSeriesMcmc rather than in the superclass TimeSeries

compound_poisson/time_series_mcmc.py
@@ -135,12 +135,24 @@ class TimeSeriesMcmc(time_series.TimeSeries):
         self.set_parameter_vector(self.parameter_mcmc[index])
         self.z_array = self.z_mcmc[index]
 
+    def forecast_self(self, n_simulation):
+        #override
+        self.read_memmap()
+        super().forecast_self(n_simulation)
+        self.del_memmap()
+
     def instantiate_forecast_self(self):
         """Override - Set the parameter from the MCMC sample
         """
         self.set_parameter_from_sample(self.self_forecaster_rng)
         forecast = super().instantiate_forecast_self()
         return forecast
+
+    def forecast(self, x, n_simulation):
+        #override
+        self.read_memmap()
+        super().forecast(n_simulation)
+        self.del_memmap()
 
     def instantiate_forecast(self, x):
         """Override - Set the parameter from the MCMC sample

compound_poisson/time_series.py
@@ -530,14 +530,12 @@ class TimeSeries(object):
         Args:
             n_simulation: number of Monte Carlo simulations
         """
-        self.read_memmap()
         if self.self_forecaster is None:
             self.self_forecaster = forecast.time_series.SelfForecaster(
                 self, self.forecaster_memmap_dir)
             self.self_forecaster.start_forecast(n_simulation)
         else:
             self.self_forecaster.resume_forecast(n_simulation)
-        self.del_memmap()
 
     def instantiate_forecast_self(self):
         """Instantiate TimeSeries for forecasting itself
@@ -569,14 +567,12 @@ class TimeSeries(object):
             x: model fields, np.array, each element for each time step
             n_simulation: number of Monte Carlo simulations
         """
-        self.read_memmap()
         if self.forecaster is None:
             self.forecaster = forecast.time_series.Forecaster(
                 self, self.forecaster_memmap_dir)
             self.forecaster.start_forecast(n_simulation, x)
         else:
             self.forecaster.resume_forecast(n_simulation)
-        self.del_memmap()
 
     def instantiate_forecast(self, x):
         """Instantiate TimeSeries for simulating the future given the current

commit 28ec0b98c975e3bb6b4771dc129646aef64b23d4

Corrected bug introduced in a0da80d, now forecasts properly

compound_poisson/time_series_mcmc.py
@@ -151,7 +151,7 @@ class TimeSeriesMcmc(time_series.TimeSeries):
     def forecast(self, x, n_simulation):
         #override
         self.read_memmap()
-        super().forecast(n_simulation)
+        super().forecast(x, n_simulation)
         self.del_memmap()
 
     def instantiate_forecast(self, x):