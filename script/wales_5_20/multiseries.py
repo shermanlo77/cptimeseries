7321793570b681bd3817239f52689588ae46a4e0
shermanlo77 <shermanip18@gmail.com>
2020-11-13 15:08:10 +0000

    Added script for multiseries

---
+++ script/wales_5_20/multiseries.py
@@ -0,0 +1,23 @@
+import argparse
+
+from numpy import random
+
+from compound_poisson import fit
+from compound_poisson import multiprocess
+import dataset
+
+def main():
+    pool = multiprocess.MPIPoolExecutor()
+
+    parser = argparse.ArgumentParser(description="Sample size")
+    parser.add_argument("--sample", help="number of mcmc samples", type=int)
+    n_sample = parser.parse_args().sample
+
+    seed = random.SeedSequence(336116686577838597869553922167649360230)
+    fitter = fit.downscale.FitterMultiSeries()
+    fitter.fit(dataset.Wales5Training(), seed, n_sample, pool)
+
+    pool.join()
+
+if __name__ == "__main__":
+    main()
